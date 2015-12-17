5a4276f752fb2d520b66ac6d42760b36f6b3943c
Bultez Jordan <jordan.bultez@gmail.com>
2015-12-17 16:39:09 +0100

    creation de la page 2 + js de base

---
+++ script.js
@@ -0,0 +1,9 @@
+function yourAge () {
+  var age = document.getElementById('age').value;
+}
+function yourHeight () {
+  var taille = document.getElementById('taille').value;
+}
+function yourWeight () {
+  var poids = document.getElementById('poids').value;
+}
